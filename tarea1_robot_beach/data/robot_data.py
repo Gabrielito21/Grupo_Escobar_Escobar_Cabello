7996d5818e3e1b3355f0f60fa62d848978172e2c
Gabriel <gee21042004@gmail.com>
2026-04-16 10:43:33 -0400

    a

--- tarea1_robot_beach/data/robot_data.py
+++ tarea1_robot_beach/data/robot_data.py
@@ -1,3 +1,4 @@
+import numpy as np
 # Estructura de datos del proyecto:
 
 # De las tablas 6, 7 , 8:
@@ -82,3 +83,21 @@ def cargar_experimentos():
     }
 
     return experimentos
+
+def generar_trayectoria_ideal(waypoints, puntos_por_segmento=100):
+    x_ideal = []
+    y_ideal = []
+    
+    for i in range(len(waypoints) - 1):
+        x0, y0 = waypoints[i]
+        x1, y1 = waypoints[i + 1]
+        
+        x_segmento = np.linspace(x0, x1, puntos_por_segmento)
+        y_segmento = np.linspace(y0, y1, puntos_por_segmento)
+        
+        x_ideal.extend(x_segmento)
+        y_ideal.extend(y_segmento)
+    
+    return np.array(x_ideal), np.array(y_ideal)
+
+
